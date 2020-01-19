0d0e9c766d6a957f066a1c040d1bc0db6c1f6f40
iholdeman1 <ian.holdeman0510@gmail.com>
2020-01-18 20:23:03 -0500

    refactored code for new data structure

--- budget_tool.py
+++ budget_tool.py
@@ -2,124 +2,148 @@ import argparse
 import csv
 import sys
 from collections import OrderedDict
+from json import dump
 
+# Constants
 MONTHLY_INCOME = 3739.0
 MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+# Sale class to neatly contain sale details
+class Sale:
+
+	def __init__(self, company, amount, occurrence):
+		self.company = company
+		self.amount = amount
+		self.occurrence = occurrence
+
+# Month class to contain sales objects and a total spent
+class Month:
+
+	def __init__(self):
+		self.sales = []
+		self.total = 0.0
+
+# Category class to contain month objects in that category
+class Category:
+
+	def __init__(self):
+		self.months = OrderedDict({month : Month() for month in MONTHS})
+		self.total = 0.0
+
+# Helper function to print data in json format for debugging purposes
+def print_data_as_json(data):
+	
+	final_result = {}
+
+	for category, obj in data.iteritems():
+		final_result[category] = OrderedDict()
+		for month in obj.months.keys():
+			final_result[category][month] = {}
+			final_result[category][month]['Total'] = obj.months[month].total
+			final_result[category][month]['Sales'] = [(sale.company, str(sale.amount), sale.occurrence) 
+																								 for sale in obj.months[month].sales]
+			final_result[category]['Total'] = obj.total
+
+	with open('data.json', 'w') as f:
+		dump(final_result, f, indent=2)
+
+# Helper function to convert money strings to floats
+def convert_to_float(s):
+	return float(s.strip().replace('$', '').replace(',', ''))
 
 # Function to read CSV file for data
 def read_data():
 	
-	data = OrderedDict()
-	headers = True
+	first_line_read = False
+	categories = {}
 
 	with open('budget.csv', 'r') as csv_file:
 		reader = csv.reader(csv_file)
-		data_headers = []
+		data_categories = []
 
 		for row in reader:
-			if headers:
+			# Read in category names
+			if not first_line_read:
 				for item in row:
 					if item:
-						item = item.decode("utf-8-sig").encode("utf-8")
-						data[item] = {}
-						for month in MONTHS:
-							data[item][month] = {}
-				headers = False
-				data_headers = data.keys()
-			
-			else:
-				count = 0
-				sub_count = 1
-				listy_list = []
-				for item in row:
-					
-					listy_list.append(item)
-
-					if sub_count % 4 == 0:
-						if listy_list[0]:
-							month = data[data_headers[count]].get(listy_list[0], {})
-							company = month.get(listy_list[1], [])
-							company.append([listy_list[2], listy_list[3]])
-							month[listy_list[1]] = company
-							data[data_headers[count]][listy_list[0]].update(month)
-
-						count += 1
-						sub_count = 0
-						listy_list = []
-
-					sub_count += 1
-
-	return data
-
-# Function to return a dictionary containing amount spent in each month
-def get_total_per_month(data): # TODO: fix months that haven't happened yet!
-
-	def convert_to_float(s):
-		return float(s.strip().replace('$', '').replace(',', ''))
-
-	monthly_data = OrderedDict()
-	for month in MONTHS:
-		monthly_data[month] = {}
-
-	for month in MONTHS:
-		total = 0.0
-		for key, val in data.iteritems():
-			if val.get(month):
-				month_data = val.get(month)
-				total += sum([convert_to_float(transactions[0]) for company in month_data for transactions in month_data.get(company)])
+						data_categories.append(item.decode("utf-8-sig").encode("utf-8"))
 				
-		monthly_data[month] = total
+				# Create category objects
+				for category in data_categories:
+					categories[category] = Category()
 
-	return monthly_data
+				# Set read
+				first_line_read = True
+			
+			# Read in row of data
+			else:
+				length = len(row)
+				index = 0
+				count = 0
+				while index != length:
+					if row[index] in MONTHS:
+						category_month = categories[data_categories[count]].months[row[index]]
+						amount_as_float = convert_to_float(row[index+2])
+						category_month.sales.append(Sale(row[index+1],amount_as_float,row[index+3]))
+						category_month.total += amount_as_float
+						categories[data_categories[count]].total += amount_as_float
+
+					index += 4
+					count += 1
+
+	return categories
 
 # Function to get total amount spent in a specified month
-def get_total_of_single_month(data, args):
+def get_total_of_single_month(data, month):
 
-	if args.month not in MONTHS:
-		raise Exception('Invalid month: {}'.format(args.month)) # TODO: Make choices list in argparse
-	
-	total = get_total_per_month(data)
-
-	if total[args.month] != 0.0:
-		print('You spent ${} in {}'.format(total[args.month], args.month))
-	else:
-		print('{} hasn\'t passed yet!'.format(args.month))
+	return sum([category.months.get(month).total for category in data.values()])
 
 # Function to get the cumulative total of a specified category
-def get_total_of_single_category(data, args):
+def get_total_of_single_category(data, category):
 
-	def convert_to_float(s):
-		return float(s.strip().replace('$', '').replace(',', ''))
+	return data[category].total
 
-	if args.category not in data:
-		raise Exception('Invalid category: {}'.format(args.category)) # TODO: Make choices list in argparse
+# Function to break down average spending for the year and in each month
+def get_average_spent_in_each_month(data):
 
-	sub_data = data.get(args.category)
-	total = 0.0
-
-	for month_data in sub_data.values():
-		for company in month_data:
-			for transaction in month_data[company]:
-				total += convert_to_float(transaction[0])
-
-	print('You spent ${} on {} related expenses this year'.format(total, args.category))
-
-# Function to break down average spending in each month
-def get_average_spent_in_each_month(data, args):
-
+	totals = OrderedDict()
 	year_total = 0.0
-	month_count = 0
-
-	total = get_total_per_month(data)
 
 	for month in MONTHS:
-		if total[month] != 0.0:
-			net_saving = MONTHLY_INCOME - total[month]
-			print('You saved ${} in {}'.format(net_saving, month))
-			year_total += net_saving
-			month_count += 1
+			totals[month] = MONTHLY_INCOME - get_total_of_single_month(data, month)
+			year_total += totals[month]
 
-	print('\nOn average you saved ${} per month this year!'.format(year_total/month_count))
+	return totals, year_total/len(MONTHS)
+
+# Intermediary function to handle arg parsing and print statements
+def handle_args(args):
+	
+	# Read data
+	data = read_data()
+
+	# Expect month command
+	if 'month' in args:
+		total = get_total_of_single_month(data, args.month)
+		if total == 0.0:
+			print('You either didn\'t spend any money or {} hasn\'t passed yet!'.format(args.month))
+		else:
+			print('You spent ${} in {}'.format(total, args.month))
+	
+	# Expect category command
+	elif 'category' in args:
+		if args.category not in data:
+			raise Exception('Invalid category: {}'.format(args.category))
+
+		total = get_total_of_single_category(data, args.category)
+
+		print('You spent ${} on {} related expenses this year'.format(total, args.category))
+	
+	# Expect average command
+	elif 'average' in args:
+		totals, average = get_average_spent_in_each_month(data)
+		for month, total in totals.iteritems():
+			print('You saved ${} in {}!'.format(total, month))
+		print('\nOn average you saved ${} per month this year!'.format(average))
 
 # Main
 def main():
@@ -128,22 +152,18 @@ def main():
 	subparsers = parser.add_subparsers(help='sub-command help')
 
 	month_parser = subparsers.add_parser('month', help='')
-	month_parser.add_argument('month', type=str, help='')
-	month_parser.set_defaults(func=get_total_of_single_month)
+	month_parser.add_argument('month', type=str, choices=MONTHS, help='')
 
 	category_parser = subparsers.add_parser('category', help='')
 	category_parser.add_argument('category', type=str, help='')
-	category_parser.set_defaults(func=get_total_of_single_category)
 
 	average_parser = subparsers.add_parser('average', help='')
-	average_parser.set_defaults(func=get_average_spent_in_each_month)
-
-	data = read_data()
+	average_parser.set_defaults(average=True)
 
 	args = parser.parse_args()
-	
+
 	try:
-		args.func(data, args)
+		handle_args(args)
 	except Exception as e:
 		print('{}'.format(e))
 		sys.exit(1)
